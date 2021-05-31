ed500b8ca49ebbf0ce874d02e993d322bea3ffba
pandablue0809 <bluep0415@gmail.com>
2021-05-31 17:43:32 +0100

    chore: Set up Express to run server and routes

---
+++ src/server/index.js
@@ -0,0 +1,34 @@
+// Empty JS object to act as endpoint for the app
+projectData = {};
+
+// Require Express to run server and routes
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+var path = require('path');
+
+app.use(express.static('dist'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+//** Main GET route **//
+app.get('/', function (req, res) {
+    res.sendFile(path.resolve('dist/index.html'))
+})
+
+//** default GET route **//
+app.get('/all', sendData);
+function sendData(req, res){    
+     res.send(projectData);
+}
+
+//** Setup Server **//
+const port = 8080;
+const server = app.listen(port, listening);
+
+function listening(){
+    console.log('Server Running');
+    console.log(`Running on localhost: ${port}`);
+}
