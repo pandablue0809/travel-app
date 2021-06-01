2651784b59061624a20b80a2bdf90721d4e0ab62
pandablue0809 <bluep0415@gmail.com>
2021-06-01 23:05:45 +0100

    chore: Set up CleanWebpackPlugin to automatically update the dist folder

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,13 +1,25 @@
 const path = require('path')
 const webpack = require('webpack')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
     output: {},
     mode: 'development',
     devtool: 'source-map',
+    stats: 'verbose',
     module: {
         rules: []
     },
-    plugins: []
+    plugins: [
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+        })
+    ]
 }
