e0f1064e1804169b27fa011405234271b46387e8
pandablue0809 <bluep0415@gmail.com>
2021-06-02 13:02:16 +0100

    chore: Set up webpack basic production environment
    The production environment is important to keep the server running
    efficiently. Specific production tools will be need to optimize the
    code and deliver content to the users in the most efficiently way.

---
+++ webpack.prod.js
@@ -0,0 +1,12 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+    entry: './src/client/index.js',
+    output: {},
+    mode: 'production',
+    module: {
+        rules: []
+    },
+    plugins: []
+}
