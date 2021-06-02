2a81d9bbf55e460ffb8e8c5660bd8b23ded07f60
pandablue0809 <bluep0415@gmail.com>
2021-06-02 13:48:23 +0100

    chore: Add Plugin to create asset references in HTML file automatically

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const webpack = require('webpack')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
 
 module.exports = {
     entry: './src/client/index.js',
@@ -26,6 +27,10 @@ module.exports = {
             // Automatically remove all unused webpack assets on rebuild
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
+        }),
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html"
         })
     ]
 }
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const webpack = require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
 
 module.exports = {
     entry: './src/client/index.js',
@@ -8,5 +9,10 @@ module.exports = {
     module: {
         rules: []
     },
-    plugins: []
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html"
+        })
+    ]
 }
