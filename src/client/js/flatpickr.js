cde1419d908188bac6cd251c1eb2f4d2814b7ccd
pandablue0809 <bluep0415@gmail.com>
2021-06-10 11:45:22 +0100

    feat: Add datepicker calendar to the date input field

---
+++ src/client/js/flatpickr.js
@@ -0,0 +1,11 @@
+import flatpickr from 'flatpickr';
+
+flatpickr('.flatpickr.js-flatpickr-dateTime', {
+    enableTime: false,
+    time_24hr: false,
+    altInput: true,
+    altFormat: 'd M Y',
+    dateFormat: 'Y-m-d',
+})
+
+export { flatpickr }
