4c1fba969c7acc21fd08eb1e310e8133a16d7754
pandablue0809 <bluep0415@gmail.com>
2021-06-21 10:20:16 +0100

    feat: Add function to update UI with an image of the entered destination

--- src/client/js/getPlaceImg.js
+++ src/client/js/getPlaceImg.js
@@ -1,14 +1,17 @@
 const getPlaceImg = async(primaryObj, baseUrl, place, apiKey)=>{
 
-    const res = await fetch(`${baseUrl}?key=${apiKey}&q=${place}&per_page=3&safesearch=true&orientation=horizontal`)
+    const res = await fetch(`${baseUrl}?key=${apiKey}&q=${place}&per_page=3&category=nature&safesearch=true&orientation=horizontal`)
 
     try{
         const apiData = await res.json();
+        //const placeImg = primaryObj.img = apiData.hits[0].largeImageURL;
         const data = primaryObj.img = apiData.hits[0].largeImageURL;
         console.log('API object received by the Pixabay function', apiData);
         return data;
+        
     }catch(error){
-        console.log('Error getting object from API', error);
+        
+        console.log('Error getting image from Pixabay', error);
     }
 }
 
